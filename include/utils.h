5e0d152246e00bffcf8884799d1aad647fe18ccd
Vang Khanh Khuyen <trinhnganquan@gmail.com>
2026-04-20 12:52:31 +0700

    update: structure of func calculateFine

--- include/utils.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <locale.h>
-
-// 123456.789 to 123,456.789
-void formatCurrency(double amount, char* outputString, size_t availableLen);
-// use: declare char outputString[], then call this function.
-// after this func operate, the value of this outputString is the desired currency format
