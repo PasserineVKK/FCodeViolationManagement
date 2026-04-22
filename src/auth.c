f21cac38c3d3d5d0a0e013c1246c66c438e07a09
Vang Khanh Khuyen <trinhnganquan@gmail.com>
2026-04-22 17:40:12 +0700

    Feat: Build raw menu for member and admin

--- src/auth.c
+++ src/auth.c
@@ -24,11 +24,11 @@ int searchMemberByIdInA(Account accounts[], int count, const char *id) {
 }
 
 //Return role of logged in account
-int login(Account accounts[], int aCount){
+int login(Account accounts[], char* studentID, int aCount){
 
     printf("===== LOGIN =====\n");
 
-	char studentID[9]; // SE000000\0
+	//char studentID[9]; // SE000000\0
 	char password[30];
 	int role;	   // 0 = Member, 1 = BOD
 	int isLocked;  // 1 = This account locked after 3 failed trials
--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include "../include/auth.h"
 #include "../include/violation.h"
 #include "../include/validate.h"
+#include "../include/consoleInput.h"
 
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
@@ -22,6 +23,114 @@ int main(int argc, char *argv[]) {
 	mCount = loadMembers(members, &mCount);
 	vCount = loadViolations(violations, &vCount);
 	aCount = loadAccounts(accounts, &aCount);
+	
+	
+	int menuRole = -1; // 
+	int choice = -1;
+	char studentID[10];
+	menuRole = login(accounts, studentID, aCount);
+	
+	switch (menuRole) {
+		case 0: {
+			do {
+				inputPosInteger(&choice, 
+				    "\n  =========================================="
+				    "\n  ¦              MEMBER MENU               ¦"
+				    "\n  =========================================="
+				    "\n  ¦  1. View Profile                       ¦"
+				    "\n  ¦  2. View Violation History             ¦"
+				    "\n  ¦  3. View Total Unpaid Fines            ¦"
+				    "\n  ¦  4. View Club Member List              ¦"
+				    "\n  ¦  5. Reset Password                     ¦"
+				    "\n  ¦  6. Log Out and Exit                   ¦"
+				    "\n  =========================================="
+				    "\n  ==> Enter your selection: "
+				);
+				
+				switch (choice){
+					case 1: 
+						//view profile
+						break;
+					case 2: 
+						//vio violation
+						break;
+					case 3: 
+						//view unpaid fine
+						break;
+					case 4: 
+						// view club memeber list
+						break;
+					case 5: 
+						//reset
+						break;
+					case 6: 
+						//log out
+						break;
+					default:
+						printf("Invalid option, please try again.");
+				}
+			} while (choice != 6);
+			break;
+		}
+		
+		case 1:{
+			do {
+				inputPosInteger(&choice, 
+				    "\n  =========================================="
+				    "\n  ¦               ADMIN MENU               ¦"
+				    "\n  =========================================="
+				    "\n  ¦  1. Add New Member                     ¦"
+				    "\n  ¦  2. Edit Member Information            ¦"
+				    "\n  ¦  3. Remove Member                      ¦"
+				    "\n  ¦  4. Record Violation                   ¦"
+				    "\n  ¦  5. Mark Fine as Paid                  ¦"
+				    "\n  ¦  6. View Violation List                ¦"
+				    "\n  ¦  7. Statistics by Department           ¦"
+				    "\n  ¦  8. Reset Member's Password            ¦"
+				    "\n  ¦  9. Reset Own Password                 ¦"
+				    "\n  ¦  10. Log Out and Exit                  ¦"
+				    "\n  =========================================="
+				    "\n  ==> Enter your selection: "
+				);
+				
+				switch (choice){
+					case 1: 
+						//add mem
+						break;
+					case 2: 
+						//edit mem
+						break;
+					case 3: 
+						//del mem
+						break;
+					case 4: 
+						//add vio
+						break;
+					case 5: 
+						//mark paid
+						break;
+					case 6: 
+						//view vio
+						break;
+					case 7: 
+						//statistic
+						break;
+					case 8:
+						// reset pass
+						break;
+					case 9: 
+						// reset pass
+						break;
+					default:
+						printf("Invalid option, please try again.");
+				}
+			} while (choice != 10);
+			break;
+		}
+		
+		case 2:
+			break; 
+	}
 	
 	return 0;
 	
