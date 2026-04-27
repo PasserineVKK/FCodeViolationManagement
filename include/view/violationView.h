30849513a0a97edf02733a13c8773ba6a95fba98
Vanbinh-dev <nguyenvanbinh542007@gmail.com>
2026-04-27 21:28:36 +0700

    Fix: Move display function to view

--- include/view/violationView.h
+++ include/view/violationView.h
@@ -2,10 +2,17 @@
 #define VIOLATION_VIEW_H
 
 #include "../violation.h"
+#include "../member.h"
 
 void displayViolationList(Violation violations[], int vCount);
 
 // Features 1.2
 void displayViolationByStudentId(char *id, Violation violations[], int vCount);
+
+// Features 2.4: Record Violation
+void recordViolationView(Violation violations[], int *vCount, Member members[], int mCount);
+
+// Features 2.5: Mark fine as paid
+void markFineAsPaidView(Violation violations[], int vCount, Member members[], int mCount);
 
 #endif
