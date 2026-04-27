00f454f4aac4c34cdbedc8aae5079ef660ba7f3f
Vanbinh-dev <nguyenvanbinh542007@gmail.com>
2026-04-27 21:26:07 +0700

    Fix: Resolve Loop dependency by create model.h file

--- include/member.h
+++ include/member.h
@@ -7,21 +7,7 @@
 #include "../include/violation.h"
 #include "../include/auth.h"
 
-
-
-typedef struct
-{
-	char fullName[100];
-	char email[100];
-	char phoneNumber[11];
-	char studentID[10]; // SE000000\0
-	int team;			// 0 = Academic, 1 = Planning, 2 = HR, 3 = Media
-	int role;			// 0 = Member, 1 = Leader/Vice, 2 = BOD
-	int violationCount;
-	int consecutiveAbsences;
-	double totalFine;
-	int isPending; //0 = not pending, 1 = pending
-} Member;
+#include "model.h"
 
 // file
 int loadMembers(Member members[], int *count);
@@ -32,14 +18,6 @@ int searchMemberByIdInM(Member members[], int count, const char *id);
 
 // Recalculates totalFine from unpaid violations, returns 1 on success
 int updateMemberTotalFine(Member members[], int mCount, Violation violations[], int vCount, const char *id);
-
-// Features 1.1
-// Display member info in table format
-void displayOneMemberInfo(Member member);
-
-// Features 1.4
-// Display member list (only name, team, role, studentID)
-void displayMemberList(Member members[], int count);
 
 // Count unpaid violations for a member
 int countUnpaidViolations(const char *id, Violation violations[], int vCount);
@@ -56,11 +34,8 @@ void updateMember(Member members[], int *mCount, Violation violations[], int vCo
 // Features 2.7: Show member list
 void showTotalFineByRole(Member members[], int count);
 
-
-
 int updateConsecutiveAbsences(Member members[], int count, const char *id);
 
-// Features 2.9: Sort by violationCount
-void displayInSortByVioCount(Member members[], int mCount, int sortMode);
+Member *getMemberById(const char *studentId, Member members[], int mCount);
 
 #endif
