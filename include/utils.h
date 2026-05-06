3c13a3807398f44683e5611826f9a8314eb4aa19
Vanbinh-dev <nguyenvanbinh542007@gmail.com>
2026-05-06 23:40:02 +0700

    Merge with feature multi-sort

--- include/utils.h
+++ include/utils.h
@@ -6,14 +6,24 @@
 #include <string.h>
 
 // 123456.789 to 123,456.789 VN�
-int formatCurrency(double amount, char* outputString, size_t availableLen);
+int formatCurrency(double amount, char *outputString, size_t availableLen);
 // use: declare char outputString[], then call this function.
 // after this func operate, the value of this outputString is the desired
 // currency format
 
-int normalizeName(char* name);
+int normalizeName(char *name);
 
-const char* translateRole(int role);
-const char* translateTeam(int team);
-const char* translateIsPaid(int isPaid);
-const char* translatePenalty(int penalty);
+const char *translateRole(int role);
+const char *translateTeam(int team);
+const char *translateIsPaid(int isPaid);
+const char *translatePenalty(int penalty);
+
+char *getLastWord(char *inputString);
+
+// declare a sortPointerList first, then use it as OUTPUT param
+// compare command:
+// reason: r DESC, R ASC
+// paid: p DESC, P ASC
+// team: t DESC, T ASC
+// valid command example: rtp, prt, PrT, tR, R, P, T, t,..
+void sortViolation(ViolationList *list, Violation *sortPointerList[], char *compareCommand);
--- src/validate.c
+++ src/validate.c
@@ -1,10 +1,14 @@
 #include "../include/validate.h"
+#include "../include/model.h"
 
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+
+#define VIOLATION_FACTORS 3
+#define MEMBER_FACTORS 3
 
 // Check integer in range
 int isIntegerBelongRange(const char *s, int min, int max)
@@ -343,4 +347,91 @@ int isBlank(const char *str)
         if (!isblank(str[i]))
             return 0;
     return 1;
+}
+
+char *getLastWord(char *inputString)
+{
+    char *output;
+    int len = strlen(inputString);
+    for (int i = len - 1; i > 0; i--)
+    {
+        if (isalpha(inputString[i]) && isspace(inputString[i - 1]))
+        {
+            output = &inputString[i];
+            return output;
+        }
+    }
+    return inputString;
+}
+
+// comparator, accept member data type
+// compare by team, role, violationCount, totalFine
+
+// comparator, accept vio data type
+// compare by team, reason, isPaid
+// command: TRP
+static int violationComparator(Violation *v1, Violation *v2, char *compareCommand)
+{
+    int len = (strlen(compareCommand) > VIOLATION_FACTORS) ? VIOLATION_FACTORS : strlen(compareCommand);
+    int result = 0;
+    for (int i = 0; i < len; i++)
+    {
+        // upper = ASC, lower = DESC
+        char currentCommand = compareCommand[i];
+        int direction = isupper(currentCommand) ? 1 : -1;
+        currentCommand = toupper(currentCommand);
+        switch (currentCommand)
+        {
+        case 'T':
+            result = v1->owner->team - v2->owner->team;
+            break;
+        case 'R':
+            result = v1->reason - v2->reason;
+            break;
+        case 'P':
+            result = v1->isPaid - v2->isPaid;
+            break;
+        default:
+            result = 0;
+        }
+        // if the first factor is equal, continue to the next, until end of commands
+        if (result != 0)
+            return direction * result;
+    }
+    return 0;
+}
+
+// idea from: struct of qsort
+void sortViolation(ViolationList *list, Violation *sortPointerList[], char *compareCommand)
+{
+    if (list == NULL || list->count <= 1)
+        return;
+    int count = list->count;
+    for (int i = 0; i < count; i++)
+    {
+        sortPointerList[i] = &list->data[i];
+    }
+
+    int minIndex = 0, maxIndex = count - 1;
+    int start = 0, end = count - 1;
+    for (int i = 0; i < count - 1; i++)
+    {
+        int minIndex = i;
+
+        for (int j = i + 1; j < count; j++)
+        {
+            if (violationComparator(sortPointerList[j], sortPointerList[minIndex], compareCommand) < 0)
+            {
+                minIndex = j;
+            }
+        }
+
+        // Swap
+        if (minIndex != i)
+        {
+            Violation *temp = sortPointerList[minIndex];
+            sortPointerList[minIndex] = sortPointerList[i];
+            sortPointerList[i] = temp;
+        }
+    }
 }
